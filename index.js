e11254ceed44d8bda0c1cc09fb8caa53871259d9
Praveen S <praveen.sri27@gmail.com>
2024-12-23 20:51:12 +0530

    testing, how to publish npm packages in public

---
+++ index.js
@@ -0,0 +1,7 @@
+const add2Numbers = (a, b) => {
+  const result = a + b;
+  console.log("Sum of 2 numbers", result);
+  return result;
+};
+
+module.exports = add2Numbers;
---
+++ test.js
@@ -0,0 +1,2 @@
+const add2Num = require("simple-add2numbers");
+console.log(add2Num(3, 2));
